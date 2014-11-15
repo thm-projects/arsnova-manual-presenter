22743ff7ded38f062233755ce579a68f8fedd33b
texnixe <info@texniq.de>
2014-11-15 22:15:25 +0100

    deletions, additions, modifications+

--- site/languages/de.php
+++ site/languages/de.php
@@ -1,3 +1,7 @@
 <?php
 l::set('ansicht', 'Ansicht ändern');
+l::set('session', 'Session vorbereiten');
+l::set('feedback', 'Im Hörsaal: Live Feedback');
+l::set('pi', 'Im Hörsaal: Peer Instruction');
+l::set('jitt', 'Vor der Vorlesung: Just in Time Teaching');
 ?>
--- site/languages/en.php
+++ site/languages/en.php
@@ -1,3 +1,7 @@
 <?php
 l::set('ansicht', 'Toggle View');
+l::set('session', 'Prepare Session');
+l::set('feedback', 'At the lecture: Live Feedback');
+l::set('pi', 'At the lecture: Peer Instruction');
+l::set('jitt', 'Before the lecture: Just in Time Teaching');
 ?>
